61180423ded0abbcb322c634e7ed82803dbd18dc
Manoj PN <manojpnarayann@gmail.com>
2026-01-21 21:39:29 +0530

    Time: 13 ms (22.49%), Space: 60.6 MB (11.77%) - LeetHub

--- 0136-single-number/0136-single-number.js
+++ 0136-single-number/0136-single-number.js
@@ -4,12 +4,12 @@
  */
 var singleNumber = function(nums) {
    let obj=nums.reduce((acc,num)=>{
-    acc[num]=(acc[num]+1)|0;
+    acc[num]=(acc[num]|0)+1;
     return acc
    },{});
-   console.log(obj)
+   
    for (let x in obj){
-    if(obj[x]===0){
+    if(obj[x]===1){
         return Number(x)
     }
    }
