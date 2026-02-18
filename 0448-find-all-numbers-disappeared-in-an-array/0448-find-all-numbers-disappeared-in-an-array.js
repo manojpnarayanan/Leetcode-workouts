b2675b578f9cec0e7c5ad5aaa3f4ff581290cd89
Manoj PN <manojpnarayann@gmail.com>
2026-02-18 17:01:28 +0530

    Time: 20 ms (43.09%), Space: 76.9 MB (23.01%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.js
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.js
@@ -3,11 +3,8 @@
  * @return {number[]}
  */
 var findDisappearedNumbers = function(nums) {
-    // let n=Math.max(...nums);
     let num=new Set(nums);
-    console.log(num)
     let out=[]
-    // console.log(n)
     for(let i=1;i<=nums.length;i++){
         if(!num.has(i)){
             out.push(i)
