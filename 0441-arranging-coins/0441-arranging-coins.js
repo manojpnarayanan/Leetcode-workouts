472e368c445f714018e017640bbf51ff6da71dd3
Manoj PN <manojpnarayann@gmail.com>
2026-03-14 09:43:23 +0530

    Time: 13 ms (16.71%), Space: 56 MB (78.33%) - LeetHub

---
+++ 0441-arranging-coins/0441-arranging-coins.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var arrangeCoins = function(n) {
+    let stair=0;
+    let count=0;
+    for (let i=1;i<=n;i++){
+        count+=i;
+        if(count<=n){
+            stair++
+        }else{
+            break;
+        }
+    };
+     return stair
+};
