bea68c9fc781f0af90f28b28f9dd851776f5cd65
Manoj PN <manojpnarayann@gmail.com>
2026-03-17 15:14:40 +0530

    Time: 28 ms (81.12%), Space: 61.6 MB (86.36%) - LeetHub

--- 3760-maximum-substrings-with-distinct-start/3760-maximum-substrings-with-distinct-start.js
+++ 3760-maximum-substrings-with-distinct-start/3760-maximum-substrings-with-distinct-start.js
@@ -3,6 +3,15 @@
  * @return {number}
  */
 var maxDistinct = function(s) {
-     return new Set(s).size
+    //  return new Set(s).size
+    let distinct='';
+    const hasStr=new Set();
+    for(let i=0;i<s.length;i++){
+        if(!hasStr.has(s[i])){
+            hasStr.add(s[i]);
+            distinct+=s[i]
+        }
+    }
+    return distinct.length
     
 };
